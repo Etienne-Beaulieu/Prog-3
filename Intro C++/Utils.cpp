b71088487d38cd8e8eece936661c0af4e6a2d29d
Etienne-Beaulieu <124171368+Etienne-Beaulieu@users.noreply.github.com>
2026-01-20 16:43:50 -0500

    Delete Intro C++ directory

--- Intro C++/Utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "Utils.h"
-
-void swap(std::vector<int> tabNombre, int index1, int index2)
-{
-	int temp = tabNombre[index1];
-	tabNombre[index1] = tabNombre[index2];
-	tabNombre[index2] = temp;
-}
--- Intro C++/Utils.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <vector>
-
-void swap(std::vector<int>, int, int);
